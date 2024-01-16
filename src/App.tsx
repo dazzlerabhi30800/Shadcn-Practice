cb9170181a256bc613b425dae937e04ed36046f8
dazzlerabhi30800 <abhisingh30800@gmail.com>
2024-01-16 11:17:41 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Using Shadcn in React.</h1>
+      <Button>Shadcn Button Component</Button>
+    </>
+  );
+}
+
+export default App;
